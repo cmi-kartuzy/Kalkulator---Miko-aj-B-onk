13a2b1fbd071bbe6ef8bcc40859b26a7c9bb0c63
mikupapi <70937889+mikupapi@users.noreply.github.com>
2020-10-08 22:12:17 +0200

    Update quiz_krzysztof_gosz.cpp

--- quiz_krzysztof_gosz.cpp
+++ quiz_krzysztof_gosz.cpp
@@ -5,212 +5,92 @@ using namespace std;
 int main() 
 
 { 
-
  int x, y, z, a; 
-
 srand(time(NULL)); 
 
 a=0; 
 
- 
- 
-
- 
- 
-
 for(int i=1;i<11;i++) 
-
 { 
-
 x=rand(); 
-
 x=x%11; 
-
 cout<<x; 
-
-cout<< "*"; 
-
-y=rand(); 
-
+cout<< "*";
+y=rand();
 y=y%11; 
-
 cout<<y; 
-
 cout<<"="; 
-
 cout<<"?"<<"\n"; 
-
 cin>>z; 
 
- 
- 
-
    if(z==x*y) 
-
    { 
-
     cout<<"To jest poprawna odpowiedź!:)"<<"\n"; 
-
     a++; 
-
    } 
-
    else 
-
    { 
-
       cout<<"Zła odpowiedź:/"<<"\n" ; 
-
    } 
-
 } 
 
-  
-
  if (a==0) 
-
- {cout<<"Zdobyłeś "; 
-
+ {
+ cout<<"Zdobyłeś "; 
  cout<<a ; 
-
-  cout<<" punktów"; 
-
+ cout<<" punktów"; 
  } 
-
  else 
-
  { 
-
-      if (a==1) 
-
- {cout<<"Zdobyłeś "; 
-
+ 
+  if (a==1) 
+ {
+ cout<<"Zdobyłeś "; 
  cout<<a ; 
-
-  cout<<" punkt"; 
-
+ cout<<" punkt"; 
  } 
-
    else 
-
    { 
-
         if (a==2) 
-
- { 
-
-     cout<<"Zdobyłeś "; 
-
- cout<<a ; 
-
-  cout<<" punkty"; 
-
+  { 
+  cout<<"Zdobyłeś "; 
+  cout<<a ; 
+  cout<<" punkty";
  } 
-
       else 
-
       { 
-
  if (a==3) 
-
  { 
-
-     cout<<"Zdobyłeś "; 
-
+ cout<<"Zdobyłeś "; 
  cout<<a ; 
-
-  cout<<" punkty"; 
-
+ cout<<" punkty"; 
  } 
-
-      else 
-
-      { 
-
- if (a==4) 
-
- { 
-
-     cout<<"Zdobyłeś "; 
-
- cout<<a ; 
-
-  cout<<" punkty"; 
-
- } 
-
      else 
-
+      { 
+ if (a==4) 
+ { 
+ cout<<"Zdobyłeś "; 
+ cout<<a ; 
+ cout<<" punkty"; 
+ } 
+     else 
      { 
-
       if (a>=5) 
-
  { 
 
-     cout<<"Zdobyłeś "; 
-
- cout<<a ; 
-
+  cout<<"Zdobyłeś "; 
+  cout<<a ;
   cout<<" punktów"; 
 
  } 
-
-          
-
-          
-
-          
-
-          
 
      } 
 
-           
-
-           
-
-           
-
-           
-
       } 
 
-           
-
-           
-
-           
-
       } 
-
-        
-
-        
-
-        
-
-   } 
-
-    
-
-    
-
-      
-
-      
-
+       
+   }
  } 
-
     return 0; 
-
-     
-
- 
- 
-
- 
- 
-
 } 
-
- 
